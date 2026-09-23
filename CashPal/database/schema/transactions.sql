c9dd63fd9e644d247ac1cf813299d1fcebe4efbc
Christian <chrisbarroso32@gmail.com>
2026-09-23 13:16:53 -0600

    Se añadió el schema de transactions

---
+++ CashPal/database/schema/transactions.sql
@@ -0,0 +1,12 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE transactions(
+    id INTEGER PRIMARY KEY,
+    sender_id INTEGER,
+    recipient_id INTEGER,
+    memo TEXT NOT NULL,
+    amount INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+);
+
+pragma table_info('transactions');
