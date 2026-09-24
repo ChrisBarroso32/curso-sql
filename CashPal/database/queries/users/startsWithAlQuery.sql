d7805818bd2fbafa76d37deea84d1d7f8a7c1b12
Christian <chrisbarroso32@gmail.com>
2026-09-24 12:45:08 -0600

    Nueva query para la tabla users: devuelve las filas que cuabran 5 carcteres en el nombre del usuario y que empiecen con 'Al'

--- CashPal/database/queries/users/startsWithAlQuery.sql
+++ CashPal/database/queries/users/startsWithAlQuery.sql
@@ -4,3 +4,11 @@ SELECT *
 FROM users
 WHERE name 
 LIKE 'Al%';
+
+-- Muestra todos los usuarios con 5 carcateres
+-- exactos en su nombre y los primeros dos empiezan
+-- con 'Al'
+SELECT *
+FROM users
+WHERE name 
+LIKE 'Al___';
