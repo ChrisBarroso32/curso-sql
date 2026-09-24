dfef45cb963086ced46eb052f6d4e8782715a955
Christian <chrisbarroso32@gmail.com>
2026-09-24 10:12:01 -0600

    Se añadieron migraciones para  las tablas users y transactions

---
+++ CashPal/database/migrations/usersMigrations.sql
@@ -0,0 +1,8 @@
+-- Cambiar el nombre de la tabla de "people" a "users"
+ALTER TABLE people RENAME TO users;
+
+-- Cambiar la columana "handle" a "username"
+ALTER TABLE users RENAME COLUMN handle TO username;
+
+-- Añadir una nueva columna "password" (TEXT) a la tabla "users"
+ALTER TABLE users ADD COLUMN password TEXT;
